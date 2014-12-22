b972aac2ec913ae61c1ef4dd287167b73f5da9d4
pilapodapostache <pilapodapostache@none>
2014-12-21 20:32:08 -0600

    Added some configurations to the LWJGLApplicationConfiguration in DesktopLauncher.java

--- what-is-going-on-desktop/src/com/wigo/game/desktop/DesktopLauncher.java
+++ what-is-going-on-desktop/src/com/wigo/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.wigo.game.WhatIsGoingOn;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "What Is Going On?";
+		config.width = 640;
+		config.height = 480;
 		new LwjglApplication(new WhatIsGoingOn(), config);
 	}
 }
